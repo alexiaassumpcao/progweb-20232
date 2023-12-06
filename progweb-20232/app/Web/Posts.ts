42684c248b6402e59b1b98aeb18a2de6eb93eebe
rueljessica <andreia_ruel@hotmail.com>
2023-12-06 12:17:42 -0300

    search field, fav button and thumb input

--- progweb-20232/app/Web/Posts.ts
+++ progweb-20232/app/Web/Posts.ts
@@ -3,33 +3,32 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { CreatePostService } from '../API/Post/utils'
 
 export default class WebCustomPostController {
-    public async show({ view, auth, params } : HttpContextContract) {
-        if (auth.isAuthenticated) {
-            const svc = CreatePostService()
-      
-            const post = await svc.getPostByID(params.id)
-            return view.render('posts/show', { post: post })
-          }
-    }
+  public async show({ view, auth, params } : HttpContextContract) {
+    if (auth.isAuthenticated) {
+      const svc = CreatePostService()
 
-    public async list({ view, auth, params } : HttpContextContract) {
-        if (auth.isAuthenticated) {
-            const svc = CreatePostService()
-            const posts = await svc.listPosts({ user_id: String(auth.user?.user_id) })
-        
-            return view.render('posts/posts', { posts: posts })
-          }
-        if (auth.isAuthenticated) {
-            const svc = CreatePostService()
-      
-            const post = await svc.getPostByID(params.id)
-            return view.render('posts/show', { post: post })
-          }
+      const post = await svc.getPostByID(params.id)
+      return view.render('posts/show', { post: post })
+      }
+  }
+
+  public async list({ view, auth, params } : HttpContextContract) {
+    if (auth.isAuthenticated) {
+      const svc = CreatePostService()
+      const posts = await svc.listPosts({ user_id: String(auth.user?.user_id) })
+  
+      return view.render('posts/posts', { posts: posts })
     }
+    if (auth.isAuthenticated) {
+      const svc = CreatePostService()
+
+      const post = await svc.getPostByID(params.id)
+      return view.render('posts/show', { post: post })
+      }
+  }
 
     public async favList({ view }: HttpContextContract) {
-            return view.render('posts/fav-posts')
-          
+      return view.render('posts/fav-posts')
     }
 
 }
--- progweb-20232/start/routes.ts
+++ progweb-20232/start/routes.ts
@@ -25,33 +25,33 @@ Route.get('/health', async ({ response }) => {
 
 // routes related to auth
 Route.get('/login', webCustomUserController.login).as('webauth.login')
-Route.post('/login', customAuthController.login).as('auth.login')
-Route.post('/logout', customAuthController.logout).as('auth.logout')
+Route.post('/api/login', customAuthController.login).as('auth.login')
+Route.post('/api/logout', customAuthController.logout).as('auth.logout')
 Route.get('/auth', webCustomUserController.createAuth).as('webauth.create')
-Route.post('/auth', customAuthController.create).as('auth.create')
+Route.post('/api/auth', customAuthController.create).as('auth.create')
 
 // routes related to users
 Route.get('/register', webCustomUserController.register).as('webauth.register')
-Route.post('/register', customUserController.create).as('user.create')
-Route.post('/register/:id', customUserController.update).as('user.patch')
-Route.delete('/register/:id', customUserController.deleteByID).as('user.delete')
-Route.get('/users', customUserController.list).as('user.list')
-Route.get('/users/:id', customUserController.getByID).as('user.getById')
+Route.post('/api/register', customUserController.create).as('user.create')
+Route.post('/api/register/:id', customUserController.update).as('user.patch')
+Route.delete('/api/register/:id', customUserController.deleteByID).as('user.delete')
+Route.get('/api/users', customUserController.list).as('user.list')
+Route.get('/api/users/:id', customUserController.getByID).as('user.getById')
 Route.get('/profile',webCustomUserController.show).as('profile.show')
 
 // routes related to posts
 Route.get('/favposts', webCustomPostController.favList).as('fav.list')
-Route.post('/posts', customPostController.create).as('post.create')
+Route.post('/api/posts', customPostController.create).as('post.create')
 Route.get('/posts', webCustomPostController.list).as('posts.list')
-Route.get('/posts', customPostController.list).as('post.list')
+Route.get('/api/posts', customPostController.list).as('post.list')
 Route.get('/posts/:id', webCustomPostController.show).as('posts.show')
-Route.put('/posts/:id', customPostController.update).as('post.update')
-Route.get('/posts/:id', customPostController.getByID).as('post.getById')
-Route.delete('/posts/:id', customPostController.deleteByID).as('post.delete')
+Route.put('/api/posts/:id', customPostController.update).as('post.update')
+Route.get('/api/posts/:id', customPostController.getByID).as('post.getById')
+Route.delete('/api/posts/:id', customPostController.deleteByID).as('post.delete')
 
 // fav posts routes
-Route.put('/posts/:id/fav-posts', customPostController.favPost).as('favpost.put')
-Route.get('/posts/fav-posts', customPostController.listFavPosts).as('favpost.list')
-Route.get('/posts/:id/fav-posts', customPostController.isFavPost).as('favpost.is')
+Route.put('/api/posts/:id/fav-posts', customPostController.favPost).as('favpost.put')
+Route.get('/api/posts/fav-posts', customPostController.listFavPosts).as('favpost.list')
+Route.get('/api/posts/:id/fav-posts', customPostController.isFavPost).as('favpost.is')
 
 
